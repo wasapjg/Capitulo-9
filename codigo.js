403b772532588b18c0dd467dfc7cc0c8015e5f28
Ponce de León Federico <federicopdl2@gmail.com>
2021-04-21 12:33:00 -0300

    cambio en el codigo js

--- codigo.js
+++ codigo.js
@@ -2,12 +2,12 @@ const nombre = document.getElementById("Nombre");
 const email = document.getElementById("Email");
 const materia = document.getElementById("Materia");
 const boton = document.getElementById("Mesa")
-const resultado = document.querySelector("resultado")
+const resultado = document.querySelector(".Resultado")
 
 boton.addEventListener("click",(e)=>{
 	e.preventDefault();
-	let error = validarCampos();
-	if(error){
+	const error = validarCampos();
+	if(error[0]){
 		resultado.innerHTML = error[1];
 		resultado.classList.add("red")
 	} else {
@@ -16,29 +16,22 @@ boton.addEventListener("click",(e)=>{
 	}
 })
 
+const validarCampos = () => {
+  if (Nombre.value.length < 5)
+    return [true, "El nombre no puede contener menos de 5 caracteres."];
 
-const validarCampos = ()=> {
-	let error = [];
-	if (Nombre.value.length < 5) {
-		error[0] = true;
-		error[1] = "El nombre no puede contener menos de 5 caracteres."
-		return error;
-	}else if (Nombre.value.length > 40) {
-		resultado.error[0] = true;
-		resultado.error[1] = "El nombre no puede contener mas de 40 caracteres."
-		return error;
-	}else if (	Email.value.length < 5 ||
-	 			Email.value.length > 40 || 
-				Email.value.indexOf("@") == -1 ||
-				Email.value.indexOf(".") == -1 )  {
-		resultado.error[0]= true;
-		resultado.error[1]= "El mail es inaválido"
-		return error;
-	}
+  if (Nombre.value.length > 40)
+    return [true, "El nombre no puede contener mas de 40 caracteres."];
+
+  if (
+    Email.value.length < 5 ||
+    Email.value.length > 40 ||
+    Email.value.indexOf("@") == -1 ||
+    Email.value.indexOf(".") == -1
+  )
+    return [true, "El mail es inaválido"];
+
+  return [false, null];
+};
 
 
-
-
-	error [0] = false;
-	return error;
-}
